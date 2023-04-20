0b9d0ed927359b6be3ca16baf912d8775e42549d
endyd1567 <endyd1567@gmail.com>
2023-04-20 10:18:40 +0900

    1. 사용자 추가 기능

--- src/test/java/portalService/test2/user/UserDaoTest.java
+++ src/test/java/portalService/test2/user/UserDaoTest.java
@@ -22,6 +22,27 @@ class UserDaoTest {
         assertThat(user.getName()).isEqualTo(name);
         assertThat(user.getPassword()).isEqualTo(password);
 
+    }
+
+    @Test
+    public void insert() throws SQLException {
+
+        User user = new User();
+        String name = "엄두용";
+        String password = "1234";
+        user.setName(name);
+        user.setPassword(password);
+
+        UserDao userDao = new UserDao();
+        userDao.insert(user);
+
+        User insertedUser = userDao.findById(user.getId());
+
+        assertThat(insertedUser.getId()).isGreaterThan(1l);
+        assertThat(insertedUser.getId()).isEqualTo(user.getId());
+        assertThat(insertedUser.getName()).isEqualTo(user.getName());
+        assertThat(insertedUser.getPassword()).isEqualTo(user.getPassword());
+
 
     }
 
